04028e7e6e5e31c8bef8b3490ddfc4e4adec2d26
warnercibertec <warnercibertec@hotmail.com>
2019-05-20 05:20:58 -0500

    proyecto jsf

---
+++ src/main/java/pe/edu/cibertec/jsf/model/Producto.java
@@ -0,0 +1,47 @@
+package pe.edu.cibertec.jsf.model;
+
+public class Producto {
+
+	private int codigo;
+	private String nombre;
+	private double precio;
+	private int cantidad;
+	private String empresa;
+	
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	
+	
+	
+	
+	
+}
